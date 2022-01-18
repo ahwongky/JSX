7b88983c78927fe9caac2ae668d0fe4a771ff3c9
ahwongky <ahwongky@gmail.com>
2022-01-18 09:30:11 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(<h1>Hello </h1>, document.getElementById("root"));
+
+/* same as the code above
+var h1= document.createElement("h1");
+h1.innerHTML = "Hello World";
+document.getElementById("root").appendChild(h1);
+*/
