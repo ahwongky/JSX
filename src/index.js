f6f47786fd755bfbccacae0bdcdf6deebad78032
ahwongky <ahwongky@gmail.com>
2022-01-18 11:06:53 +0800

    JSX commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+/* same as the code above
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-ReactDOM.render(<h1>Hello </h1>, document.getElementById("root"));
+*/
+const fname = "Nicki";
+
+const lname = "Wong";
+
+const num = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fname} {lname}!
+    </h1>
+    <h1>Hello {fname + " " + lname}!</h1>
+    <h1>Hello {`${fname}  ${lname}`} !</h1>
+    <p>
+      {" "}
+      test {num}
+      {Math.floor(Math.random() * 10)}
+    </p>
+
+    <h1>My Favourit Foods </h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Noodles</li>
+      <li>Jamon</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
 
 /* same as the code above
 var h1= document.createElement("h1");
